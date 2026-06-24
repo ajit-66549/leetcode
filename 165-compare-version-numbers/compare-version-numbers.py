d24f2c312bbad277cc23d595e9475b75096fd483
Ajit Chaudhary <ajitchaudhary6793@gmail.com>
2026-06-24 10:32:00 -0500

    Time: 0 ms (100.00%) | Memory: 19.3 MB (26.31%) - LeetSync

---
+++ 165-compare-version-numbers/compare-version-numbers.py
@@ -0,0 +1,27 @@
+class Solution:
+    def compareVersion(self, version1: str, version2: str) -> int:
+        # Time complexity: O(n) and Space complexity: O(1)
+        i = j = 0
+
+        while i < len(version1) or j < len(version2):
+            num1 = 0
+            while i < len(version1) and version1[i] != ".":
+                num1 = num1 * 10 + int(version1[i])
+                i += 1
+
+            num2 = 0
+            while j < len(version2) and version2[j] != ".":
+                num2 = num2 * 10 + int(version2[j])
+                j += 1
+
+            if num1 < num2:
+                return -1
+            elif num1 > num2:
+                return 1
+
+            if i < len(version1) and version1[i] == ".":
+                i += 1
+            if j < len(version2) and version2[j] == ".":
+                j += 1
+
+        return 0
