cb1a865287ada27fa008194514a776cd8993c7a0
Ajit Chaudhary <ajitchaudhary6793@gmail.com>
2026-07-19 12:52:03 -0500

    Time: 0 ms (100.00%) | Memory: 19.5 MB (16.99%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.py
+++ 63-unique-paths-ii/unique-paths-ii.py
@@ -1,31 +1,20 @@
 class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
-        # Time and Space complexity: O(rows * columns)
+        # Time complexity: O(rows * columns) and Space complexity: O(columns)
         if obstacleGrid[0][0] == 1:
             return 0
 
         rows = len(obstacleGrid)
         columns = len(obstacleGrid[0])
 
-        dp = [[0] * columns for _ in range(rows)]
-        dp[0][0] = 1
+        dp = [0] * columns
+        dp[0] = 1
 
-        # initialize first row
-        for column in range(1, columns):
-            if obstacleGrid[0][column] == 0:
-                dp[0][column] = dp[0][column-1]
-            
-        # initialize first column
-        for row in range(1, rows):
-            if obstacleGrid[row][0] == 0:
-                dp[row][0] = dp[row-1][0]
-
-        # calculate remaining cells
-        for row in range(1, rows):
-            for column in range(1, columns):
+        for row in range(rows):
+            for column in range(columns):
                 if obstacleGrid[row][column] == 1:
-                    dp[row][column] = 0
-                else:
-                    dp[row][column] = dp[row-1][column] + dp[row][column-1]
+                    dp[column] = 0
+                elif column > 0:
+                    dp[column] = dp[column] + dp[column-1]
 
-        return dp[rows-1][columns-1]
+        return dp[-1]
