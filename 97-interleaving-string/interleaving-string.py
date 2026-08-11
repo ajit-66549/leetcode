ec5c32290cb5bef1788b6c815ac40f3ef04aa5df
Ajit Chaudhary <ajitchaudhary6793@gmail.com>
2026-08-11 00:59:52 -0500

    Time: 51 ms (59.33%) | Memory: 19.4 MB (72.89%) - LeetSync

--- 97-interleaving-string/interleaving-string.py
+++ 97-interleaving-string/interleaving-string.py
@@ -1,24 +1,27 @@
 class Solution:
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
-        # Time and Space complexity: O(m*n)
+        # Time complexity: O(m*n) and Space complexity: O(n)
         m = len(s1)
         n = len(s2)
 
         if len(s3) != m+n:
             return False
 
-        dp = [[False] * (n+1) for _ in range(m+1)]
-        dp[m][n] = True
+        dp = [False] * (n+1)
+        dp[n] = True
 
         for i in range(m, -1, -1):
             for j in range(n, -1, -1):
                 if i == m and j == n:
                     continue
 
-                if (i < m and s1[i] == s3[i+j]):
-                    dp[i][j] = dp[i+1][j]
+                current = False
+                if i < m and s1[i] == s3[i+j]:
+                    current = dp[j]
 
-                if (j < n and s2[j] == s3[i+j]):
-                    dp[i][j] = dp[i][j] or dp[i][j+1]
+                if j < n and s2[j] == s3[i+j]:
+                    current = current or dp[j+1]
+                
+                dp[j] = current
 
-        return dp[0][0]
+        return dp[0]
