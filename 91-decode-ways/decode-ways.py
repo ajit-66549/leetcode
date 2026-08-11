cda142d4428e291e879b94b2a8dd95666b09e399
Ajit Chaudhary <ajitchaudhary6793@gmail.com>
2026-08-10 20:43:19 -0500

    Time: 1 ms (34.40%) | Memory: 19.2 MB (70.70%) - LeetSync

---
+++ 91-decode-ways/decode-ways.py
@@ -0,0 +1,15 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        # Time and Space complexity: O(n)
+        dp = [0] * (len(s) + 1)
+        dp[len(s)] = 1
+
+        for i in range(len(s) - 1, -1, -1):
+            if s[i] != "0":
+                dp[i] = dp[i+1]
+
+            if i + 1 < len(s):
+                if "10" <= s[i:i+2] <= "26":
+                    dp[i] += dp[i+2]
+
+        return dp[0]
