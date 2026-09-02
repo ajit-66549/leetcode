48d5d279e0dfb3622bed0128c9e09372fa9db481
Ajit Chaudhary <ajitchaudhary6793@gmail.com>
2026-09-02 14:43:51 -0500

    Time: 7 ms (43.59%) | Memory: 20.4 MB (78.69%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -1,31 +1,33 @@
 from collections import defaultdict, deque
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        # Time and Space complexity: O(n+m) where n = numCourses and m = len(prerequisites)
         indegree = [0] * numCourses
         graph = defaultdict(list)
+
         answer = []
 
-        for i in range(len(prerequisites)):
-            course = prerequisites[i][0]
-            pre = prerequisites[i][1]
-
+        for course, pre in prerequisites:
             indegree[course] += 1
             graph[pre].append(course)
 
-        queue = deque()
+        queue = deque()  # queue holds the courses which can be taken
 
+        # take the courses which has 0 prerequisites and add in queue
         for course in range(numCourses):
             if indegree[course] == 0:
                 queue.append(course)
 
-        while queue:
+        while queue:  # process the course as it can be pre of other course
             pre = queue.popleft()
             answer.append(pre)
 
-            for course in graph[pre]:
+            # get the courses depended on this 
+            courses = graph[pre]
+            for course in courses:
                 indegree[course] -= 1
 
-                if indegree[course] == 0:
+                if indegree[course] == 0: # this course can be taken, so add in queue
                     queue.append(course)
-                    
+
         return len(answer) == numCourses
