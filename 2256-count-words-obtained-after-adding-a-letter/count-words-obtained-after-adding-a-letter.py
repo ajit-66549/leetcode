e14ae250431f0d106685d9542a579c00ff64b0e8
Ajit Chaudhary <ajitchaudhary6793@gmail.com>
2026-09-12 01:00:26 -0500

    Time: 373 ms (54.55%) | Memory: 34.7 MB (60.84%) - LeetSync

--- 2256-count-words-obtained-after-adding-a-letter/count-words-obtained-after-adding-a-letter.py
+++ 2256-count-words-obtained-after-adding-a-letter/count-words-obtained-after-adding-a-letter.py
@@ -1,18 +1,26 @@
 class Solution:
     def wordCount(self, startWords: List[str], targetWords: List[str]) -> int:
+        # Time complexity: O(Sm + Tm) and Space complexity: O(S), S = len(startWords) and m = maximum word length
         start_set = set()
         count = 0
         for word in startWords:
-            sorted_word = "".join(sorted(word))
-            start_set.add(sorted_word)
+            mask = 0
+            for letter in word:
+                bit = ord(letter) - ord('a')
+                mask |= (1 << bit)  # turn one each letter in word
+            start_set.add(mask)
 
         for word in targetWords:
-            sorted_word = "".join(sorted(word))
+            mask = 0
+            for letter in word:
+                bit = ord(letter) - ord('a')
+                mask |= (1 << bit)  # turn one each letter in word
 
-            for i in range(len(sorted_word)):
-                new_word = sorted_word[:i] + sorted_word[i+1:]
-                 
-                if new_word in start_set:
+            for letter in word:
+                bit = ord(letter) - ord('a')
+                new_mask = mask ^ (1 << bit)  # XOR operation to remove the letter
+
+                if new_mask in start_set:
                     count += 1
                     break
         
